3ec7f7586b355b80af7284d727f3271d1668d7c9
pb-bennett <97233485+pb-bennett@users.noreply.github.com>
2024-07-01 21:16:22 +0200

    added morgan and imported and used species routes

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,22 @@
-import dotenv from 'dotenv';
-import express, { Request, Response } from 'express';
-import logger from './utils/logger';
+import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+import morgan from "morgan";
 
-import userRoutes from './routes/userRoutes';
-import plantRoutes from './routes/plantRoutes';
+import logger from "./utils/logger";
+
+import userRoutes from "./routes/userRoutes";
+import plantRoutes from "./routes/plantRoutes";
+import speciesRoutes from "./routes/speciesRoutes";
 
 dotenv.config();
 const app = express();
-const port: string = process.env.PORT || '3000';
+const port: string = process.env.PORT || "3000";
 
 app.use(express.json());
+app.use(morgan("dev"));
 
-app.use('/api/v1/users', userRoutes);
-app.use('/api/v1/plants', plantRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/plants", plantRoutes);
+app.use("/api/v1/species", speciesRoutes);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
